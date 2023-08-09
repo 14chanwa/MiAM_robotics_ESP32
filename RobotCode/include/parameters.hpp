26a826a3866011b38f339806545bdd1bc05cd10a
14chanwa <quentin.cwn@gmail.com>
2023-08-10 01:27:54 +0200

    Fixups

--- RobotCode/include/parameters.hpp
+++ RobotCode/include/parameters.hpp
@@ -26,36 +26,36 @@
 // Resistor values
 /////////////////////////////////////////////
 
-#define RESISTOR_R1 100000.0
-#define RESISTOR_R2 10000.0
+#define RESISTOR_R1 100000.0f
+#define RESISTOR_R2 10000.0f
 
 /////////////////////////////////////////////
 // Wheel specs
 /////////////////////////////////////////////
 
-#define WHEEL_RADIUS_MM 30.0
+#define WHEEL_RADIUS_MM 30.0f
 #define WHEEL_SPACING_MM 73.5
 
 /////////////////////////////////////////////
 // Motor & encoder specs
 /////////////////////////////////////////////
 
-#define MOTOR_REDUCTION_FACTOR 100.0
-#define MOTOR_RATED_RPM 220.0
+#define MOTOR_REDUCTION_FACTOR 100.0f
+#define MOTOR_RATED_RPM 220.0f
 
-#define ENCODER_PULSE_PER_REVOLUTION 28.0
-#define ENCODER_SPEED_TICK_PERIOD_MS 10.0
+#define ENCODER_PULSE_PER_REVOLUTION 28.0f
+#define ENCODER_SPEED_TICK_PERIOD_MS 10.0f
 
 // conversion functions
-#define RPM_TO_RAD_S(VALUE) (VALUE * 2.0 * M_PI / 60.0)
-#define RAD_S_TO_RPM(VALUE) (VALUE * 60.0 / (2.0 * M_PI))
+#define RPM_TO_RAD_S(VALUE) (VALUE * 2.0f * M_PI / 60.0f)
+#define RAD_S_TO_RPM(VALUE) (VALUE * 60.0f / (2.0f * M_PI))
 
 // give 20% overhead
 #define MAX_SPEED_RPM (0.8 * MOTOR_RATED_RPM)
 #define MAX_SPEED_RAD_S (RPM_TO_RAD_S(MAX_SPEED_RPM))
 
 #define MAX_WHEEL_SPEED_MM_S (MAX_SPEED_RAD_S * WHEEL_RADIUS_MM)
-#define MAX_WHEEL_ACCELERATION_MM_S 250.0
+#define MAX_WHEEL_ACCELERATION_MM_S 250.0f
 
 /////////////////////////////////////////////
 // Motion controller specs
@@ -68,24 +68,24 @@
 // Target control will be
 // PWM = targetSpeed (rpm) * 255 / max speed (rpm) * MOTOR_TARGET_CONTROL_A + MOTOR_TARGET_CONTROL_B
 #define MOTOR_TARGET_CONTROL_B 20
-#define MOTOR_TARGET_CONTROL_A 0.52
+#define MOTOR_TARGET_CONTROL_A 0.52f
 // offset will be applied if targetSpeed (rad/s) is above threshold
-#define MOTOR_ST0P_THRESHOLD_RAD_S 0.1
+#define MOTOR_ST0P_THRESHOLD_RAD_S 0.1f
 
 // Wheel PID parameters
-#define VELOCITY_KP 0.5
-#define VELOCITY_KD 0.01
-#define VELOCITY_KI 0.02
+#define VELOCITY_KP 0.5f
+#define VELOCITY_KD 0.01f
+#define VELOCITY_KI 0.02f
 
 // Motion controller PID parameters
-#define LINEAR_KP 3.5
-#define LINEAR_KD 0.01
-#define LINEAR_KI 0.00
+#define LINEAR_KP 3.5f
+#define LINEAR_KD 0.01f
+#define LINEAR_KI 0.00f
 
-#define TRANSVERSE_KP 0.005
+#define TRANSVERSE_KP 0.005f
 
-#define ROTATION_KP 5.0
-#define ROTATION_KD 0.01
-#define ROTATION_KI 0.0
+#define ROTATION_KP 5.0f
+#define ROTATION_KD 0.0f
+#define ROTATION_KI 0.0f
 
 #endif
