26a2274118b00ca25e673bca93f5ce97eb6add28
14chanwa <github.massager767@passmail.net>
2026-03-29 13:25:51 +0200

    Revert slave board to old code

--- Code/eurobot2026/WiFiSlave/WiFiSlave.ino
+++ Code/eurobot2026/WiFiSlave/WiFiSlave.ino
@@ -2,20 +2,19 @@
 #include "secret.hpp"
 #include <FreeRTOS.h>
 #include <PacketSerial.h>
-#include <PubSubClient.h>
 
 #define MAX_SEND_SIZE 512
+#define MAX_RECV_SIZE 512
+#define TCP_SERVER_PORT 778
 
 char ssid[] = WIFI_SSID;      // your network SSID (name)
 char pass[] = WIFI_PASS;      // your network password
 int status = WL_IDLE_STATUS;  // Indicator of Wifi status
 
-WiFiClient wifiClient;
+WiFiClient client;
+IPAddress serverAddress(SERVER_IP);
 
-char mqttServer[] = MQTT_SERVER_IP;
-char clientId[] = "amebaClient";
-char publishTopic[] = "pami-state";
-char subscribeTopic[] = "match-state";
+char data_wifi_rx[MAX_RECV_SIZE];
 
 // Access to data wifi tx is protected using a mutex
 // It is accessed in taskSerialMonitor and taskSendDataToServer
@@ -25,43 +24,21 @@ uint data_wifi_tx_size = 0;
 char data_wifi_tx[MAX_SEND_SIZE];
 
 PacketSerial myPacketSerial;
-PubSubClient client(wifiClient);
-
-void reconnect() {
-  // Loop until we're reconnected
-  while (!(client.connected())) {
-    Serial.print("\r\nAttempting MQTT connection...");
-    // Attempt to connect
-    byte bssid[6];
-    WiFi.BSSID(bssid);
-    char buffer[22];
-    int n;
-    n = sprintf(buffer, "bw16-%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx", bssid[5], bssid[4], bssid[3], bssid[2], bssid[1], bssid[0]);
-    printf("[%s] is a string %d chars long\n", buffer, n);
-    if (client.connect(buffer)) {
-      Serial.println("connected");
-      client.subscribe(subscribeTopic);
-    } else {
-      Serial.println("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      //Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
 
 #define LED_PIN PA30
 
-void task_blink_led(void* parameters) {
-  pinMode(LED_PIN, OUTPUT);
+void task_blink_led(void *parameters)
+{
+    pinMode(LED_PIN, OUTPUT);
 
-  for (;;) {
-    digitalWrite(LED_PIN, HIGH);
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-    digitalWrite(LED_PIN, LOW);
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-  }
+    for(;;)
+    {
+      digitalWrite(LED_PIN, HIGH);
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
+      digitalWrite(LED_PIN, LOW);
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    
 }
 
 
@@ -70,12 +47,13 @@ void setup() {
   xSemaphore = xSemaphoreCreateMutex();
 
   xTaskCreate(
-    task_blink_led,
-    "task_blink_led",
-    10000,
-    NULL,
-    1,
-    NULL);
+      task_blink_led, 
+      "task_blink_led",
+      10000,
+      NULL,
+      1,
+      NULL
+  ); 
 
   Serial.begin(115200);
   Serial1.begin(115200);
@@ -89,17 +67,12 @@ void setup() {
     status = WiFi.begin(ssid, pass);
   }
 
-  wifiClient.setBlockingMode();
-  wifiClient.setTimeout(1);
-  client.setServer(mqttServer, 1883);
-  client.setCallback(callback);
-
-  delay(1500);
-
+  client.setTimeout(1);
   // xTaskCreate(taskPrintCurrentNet, "taskPrintCurrentNet", 1000, NULL, 70, NULL);
   xTaskCreate(taskSendDataToServer, "taskSendDataToServer", 10000, NULL, 30, NULL);
   vTaskDelay(2 / portTICK_PERIOD_MS);
   xTaskCreate(taskSerialMonitor, "taskSerialMonitor", 10000, NULL, 30, NULL);
+  
 }
 
 // On serial packet received, trigger transfer over wifi
@@ -107,21 +80,27 @@ void onPacketReceived(const uint8_t* buffer, size_t size) {
   Serial.print(">>> Received serial message: size ");
   Serial.println(size);
 
-  if (xSemaphoreTake(xSemaphore, portMAX_DELAY)) {
-    for (uint i = 0; i < size; i++) {
+  if(xSemaphoreTake(xSemaphore, portMAX_DELAY))
+  {
+    for (uint i=0; i<size; i++)
+    {
       data_wifi_tx[i] = buffer[i];
     }
     data_wifi_tx_size = size;
     need_data_sent = true;
     xSemaphoreGive(xSemaphore);
-  } else {
+  }
+  else
+  {
     Serial.println("onPacketReceived: Semaphore not taken");
   }
+  
 }
 
 // Periodically print net info over serial debug
 void taskPrintCurrentNet(void* parameters) {
   for (;;) {
+    // put your main code here, to run repeatedly:
     printCurrentNet();
     vTaskDelay(10000 / portTICK_PERIOD_MS);
   }
@@ -131,59 +110,71 @@ void taskPrintCurrentNet(void* parameters) {
 void taskSendDataToServer(void* parameters) {
   for (;;) {
 
-    if (!client.connected()) {
-      reconnect();
+    if (!client.connected())
+    {
+      Serial.println(">> client not connected");
+      if (client.connect(serverAddress, TCP_SERVER_PORT)) 
+      {
+        Serial.println("connected to server");
+      }
+      else
+      {
+        Serial.println("could not connect to server");
+      }
     }
-    client.loop();
 
-    if (xSemaphoreTake(xSemaphore, portMAX_DELAY)) {
+    if(xSemaphoreTake(xSemaphore, portMAX_DELAY))
+    {
       if (need_data_sent) {
         Serial.print(">> need_data_sent:true, size: ");
         Serial.println(data_wifi_tx_size);
-        client.publish((const char*)publishTopic, (const uint8_t*)data_wifi_tx, (unsigned int)data_wifi_tx_size);
+
+        int sizeOfSent = client.write(data_wifi_tx, data_wifi_tx_size);
+        if (sizeOfSent > 0) {
+          Serial.println("[CLIENT] Send data to server successfully");
+          Serial.print("Size: ");
+          Serial.println(sizeOfSent);
+        } else {
+          Serial.println("[ERROR] Send data failed");
+        }
+
         need_data_sent = false;
       }
       xSemaphoreGive(xSemaphore);
-    } else {
+      vTaskDelay(20 / portTICK_PERIOD_MS);
+    }
+    else
+    {
       Serial.println("taskSendDataToServer: Semaphore not taken");
     }
+
+    int sizeOfData = 0;
+    while (client.available() > 0 && sizeOfData < MAX_RECV_SIZE) {
+      data_wifi_rx[sizeOfData++] = client.read();
+    }
+    if (sizeOfData < 0) {
+      Serial.println("[ERROR] Receive data failed");
+    } else if (sizeOfData > 0) {
+      Serial.print("[CLIENT] Receive data from server: ");
+      Serial.println(data_wifi_rx);
+      Serial.print("Size: ");
+      Serial.println(sizeOfData);
+
+      // Transfer data over serial
+      myPacketSerial.send((uint8_t*)data_wifi_rx, sizeOfData);
+    }
+
+    //client.stop();
     vTaskDelay(20 / portTICK_PERIOD_MS);
   }
+
 }
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (unsigned int i = 0; i < length; i++) {
-    Serial.print((char)(payload[i]));
-  }
-  Serial.println();
-
-  if (length < 0) {
-    Serial.println("[ERROR] Receive data failed");
-  } else if (length > 0) {
-    Serial.print("[CLIENT] Receive data from server: ");
-    Serial.println((const char*)payload);
-    Serial.print("Size: ");
-    Serial.println(length);
-
-    Serial.print("Received message: ");
-    for (uint i = 0; i < length; i++) {
-      Serial.print((uint8_t)payload[i]);
-      Serial.print(" ");
-    }
-    Serial.println();
-
-    // Transfer data over serial
-    myPacketSerial.send((uint8_t*)payload, (size_t)length);
-  }
-}
-
 
 // Periodically check serial
-void taskSerialMonitor(void* parameters) {
-  for (;;) {
+void taskSerialMonitor(void* parameters)
+{
+  for (;;)
+  {
     myPacketSerial.update();
     vTaskDelay(10 / portTICK_PERIOD_MS);
   }
